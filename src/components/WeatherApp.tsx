c0eaa5f70046d13bcad4b7ab43f55c60a7e61175
Mauricio Erazo <antonierazo13@hotmail.com>
2024-06-19 14:48:03 +0200

    feat: added cdnjs to get icons and images for weather

---
+++ src/components/WeatherApp.tsx
@@ -0,0 +1,16 @@
+ 
+const WeatherApp = () => {
+  return (
+    <div className="container">
+      <div className="weather-app">
+        <div className="search">
+          <div className="search-top">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherApp
