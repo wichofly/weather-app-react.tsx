f303b7d27c88150eb7b63c665480a6364acf99b9
Mauricio Erazo <antonierazo13@hotmail.com>
2024-06-24 01:39:13 +0200

    refactor: weather details moved

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,13 +1,8 @@
 import useUser from '../components/Hooks/useUser';
 import './WeatherApp.css';
 
-import sunny from '../assets/sunny.png';
-import cloudy from '../assets/cloudy.png';
-import rainy from '../assets/rainy.png';
-import snowy from '../assets/snowy.png';
-import hazy from '../assets/hazy.png';
-import misty from '../assets/misty.png';
 import loadingGif from '../assets/loading.gif';
+import WeatherDetails from './WeatherDetails';
 
 const WeatherApp = () => {
   const { data, location, setLocation, loading, updateWeather } =
@@ -29,20 +24,6 @@ const WeatherApp = () => {
     };
     return date.toLocaleDateString('en-US', options);
   };
-
-  const weatherImages: { [key: string]: string } = {
-    Clear: sunny,
-    Clouds: cloudy,
-    Rain: rainy,
-    Snow: snowy,
-    Haze: hazy,
-    Mist: misty,
-  };
-
-  const weatherImage =
-    data && data.weather
-      ? weatherImages[data.weather[0].main]
-      : 'How is the weather?';
 
   const bgImages = {
     Clear: 'linear-gradient(to right, #FFDAB9, #FF9C00)',
@@ -91,47 +72,9 @@ const WeatherApp = () => {
         ) : data?.notFound ? (
           <div className="not-found">Not Found 🤦‍♂️</div>
         ) : null}
-        {data && !data.notFound && (
-          <div className="weather">
-            <img src={weatherImage} alt="type of weather" />
-            <div className="weather-type">
-              {data.weather ? data.weather[0].main : 'Weather'}
-            </div>
-            <div className="temp">
-              {data.main ? `${Math.floor(data.main.temp)}` : '0'}°C
-            </div>
-          </div>
-        )}
-
-        <div className="weather-date">
-          <p>{getCurrentDate()}</p>
-        </div>
-
-        <div className="weather-data">
-          <div className="humidity">
-            {data && data.main ? (
-              <>
-                <div className="data-name">Humidity</div>
-                <i>💧</i>
-                <div className="data">{data.main.humidity}%</div>
-              </>
-            ) : (
-              <div className="data-name">No humidity data available</div>
-            )}
-          </div>
-
-          <div className="wind">
-            {data && data.wind ? (
-              <>
-                <div className="data-name">Wind</div>
-                <i>💨</i>
-                <div className="data">{data.wind.speed} km/h</div>
-              </>
-            ) : (
-              <div className="data-name">No wind data available</div>
-            )}
-          </div>
-        </div>
+        {data && !data.notFound ? (
+          <WeatherDetails data={data} getCurrentDate={getCurrentDate} />
+        ) : null}
       </div>
     </div>
   );
